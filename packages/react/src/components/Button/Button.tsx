f1a6ff48856eba8a71e89a0d04649ce7473918c3
Evan Fleet <evfleet@gmail.com>
2023-05-27 13:39:56 -0400

    feat(react): :sparkles: setup button component and stories

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,9 +1,17 @@
-const Button = () => {
-  return (
-    <div>
-      <button>Decor</button>
-    </div>
-  )
+import React from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+export interface ButtonProps
+	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	asChild?: boolean;
 }
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+	({ className, asChild = false, ...props }, ref) => {
+		const Component = asChild ? Slot : "button";
+
+		return <Component ref={ref} {...props} />;
+	},
+);
 
 export default Button;
