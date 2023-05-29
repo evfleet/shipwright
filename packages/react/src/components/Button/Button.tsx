af21fb096d571bd66b55b97949284347360f2de8
Evan Fleet <evfleet@gmail.com>
2023-05-29 15:12:51 -0400

    build(react): :lipstick: setup prettier and plugins

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -3,35 +3,35 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
-const buttonVariants = cva("inline-flex py-1 px-2 ", {
-	variants: {
-		intent: {
-			primary: "bg-blue-600 text-white",
-		},
-	},
-	defaultVariants: {
-		intent: "primary",
-	},
+const buttonVariants = cva("inline-flex px-2 py-1 ", {
+  variants: {
+    intent: {
+      primary: "bg-blue-600 text-white",
+    },
+  },
+  defaultVariants: {
+    intent: "primary",
+  },
 });
 
 export interface ButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-		VariantProps<typeof buttonVariants> {
-	asChild?: boolean;
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, asChild = false, ...props }, ref) => {
-		const Component = asChild ? Slot : "button";
+  ({ className, asChild = false, ...props }, ref) => {
+    const Component = asChild ? Slot : "button";
 
-		return (
-			<Component
-				className={twMerge(buttonVariants({ className }))}
-				ref={ref}
-				{...props}
-			/>
-		);
-	},
+    return (
+      <Component
+        className={twMerge(buttonVariants({ className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
 );
 
 export default Button;
