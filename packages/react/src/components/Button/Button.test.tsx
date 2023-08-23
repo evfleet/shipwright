9d58f87f3540caf19d65b45618f807b343baa34b
Evan Fleet <evfleet@gmail.com>
2023-08-22 20:50:39 -0400

    test(react): add accessibility tests to button

--- packages/react/src/components/Button/Button.test.tsx
+++ packages/react/src/components/Button/Button.test.tsx
@@ -1,10 +1,19 @@
 import { render } from "@testing-library/react";
+import { axe } from "jest-axe";
 
 import Button from "./Button";
 
 describe("Button", () => {
   it("should render successfully", () => {
     const { baseElement } = render(<Button />);
+
     expect(baseElement).toBeTruthy();
+  });
+
+  it("should have no accessibility issues", async () => {
+    const { container } = render(<Button />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
   });
 });
