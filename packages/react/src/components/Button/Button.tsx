2064f83689b1c7f933ce3d8652b6956ea16ca78a
Evan Fleet <evfleet@gmail.com>
2023-12-06 14:51:58 -0500

    feat(react): setup style merging

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { cva, VariantProps } from "class-variance-authority";
+
+import { cn } from "../../utils";
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
+const buttonVariants = cva();
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ asChild = false, children, ...props }, ref) => {
+  ({ asChild = false, children, className, ...props }, ref) => {
     const Component = asChild ? Slot : "button";
 
     return (
-      <Component ref={ref} {...props}>
+      <Component
+        className={cn(buttonVariants({ className }))}
+        ref={ref}
+        {...props}
+      >
         {children}
       </Component>
     );
