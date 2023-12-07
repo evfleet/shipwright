eefd8ab2a4a3ad7a913362b8904943bb92891dc4
Evan Fleet <evfleet@gmail.com>
2023-12-07 18:11:38 -0500

    build(react): setup storybook addon for pseudo states

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -9,6 +9,7 @@ import { dirname, join } from "path";
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -17,6 +18,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("storybook-addon-pseudo-states"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
