19bfc7e661e256c20eee28655499ae462b554c5b
Evan Fleet <evfleet@gmail.com>
2023-12-09 20:22:28 -0500

    chore(react): remove storybook addons

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -15,10 +15,8 @@ const config: StorybookConfig = {
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("storybook-addon-pseudo-states"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
